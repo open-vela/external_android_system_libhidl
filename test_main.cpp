e45b46cf3628eb0cd909111a6067afb69fcf9dd9
liujinye <liujinye@xiaomi.com>
2019-01-09 18:00:33 +0800

    Revert "Revert "Add gServiceSidMap.""
    This reverts commit 8f21ad8aa6496e2ad6061589b87275f4a98ef651.
    
    This enables getting security contexts from the kernel.
    
    Bug: 121035042
    Test: boot patch w/ w/o kernel patch on cuttlefish and check for
        security logs.
    Test: check for cnd failures on crosshatch.
    
    Change-Id: I771fa6bbeac0b9cbba5321881e191605b0e17e23
    Merged-In: I771fa6bbeac0b9cbba5321881e191605b0e17e23
    (cherry picked from commit 42bc6d55089566924dc367afa40280269b1d0419)

--- test_main.cpp
+++ test_main.cpp
@@ -17,7 +17,7 @@
 #define LOG_TAG "LibHidlTest"
 
 #include <android-base/logging.h>
-#include <android/hardware/tests/inheritance/1.0/IParent.h>
+#include <android/hidl/memory/1.0/IMemory.h>
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include <hidl/HidlSupport.h>
@@ -390,12 +390,15 @@ TEST_F(LibHidlTest, HidlVersionTest) {
     hidl_version v3_0b{3,0};
 
     EXPECT_TRUE(v1_0 < v2_0);
+    EXPECT_TRUE(v1_0 != v2_0);
     EXPECT_TRUE(v2_0 < v2_1);
     EXPECT_TRUE(v2_1 < v3_0);
     EXPECT_TRUE(v2_0 > v1_0);
+    EXPECT_TRUE(v2_0 != v1_0);
     EXPECT_TRUE(v2_1 > v2_0);
     EXPECT_TRUE(v3_0 > v2_1);
     EXPECT_TRUE(v3_0 == v3_0b);
+    EXPECT_FALSE(v3_0 != v3_0b);
     EXPECT_TRUE(v3_0 <= v3_0b);
     EXPECT_TRUE(v2_2 <= v3_0);
     EXPECT_TRUE(v3_0 >= v3_0b);
@@ -459,12 +462,14 @@ TEST_F(LibHidlTest, StatusStringTest) {
 
 TEST_F(LibHidlTest, PreloadTest) {
     using ::android::hardware::preloadPassthroughService;
-    using ::android::hardware::tests::inheritance::V1_0::IParent;
+    using ::android::hidl::memory::V1_0::IMemory;
 
-    static const std::string kLib = "android.hardware.tests.inheritance@1.0-impl.so";
+    // installed on all devices by default in both bitnesses and not otherwise a dependency of this
+    // test.
+    static const std::string kLib = "android.hidl.memory@1.0-impl.so";
 
     EXPECT_FALSE(isLibraryOpen(kLib));
-    preloadPassthroughService<IParent>();
+    preloadPassthroughService<IMemory>();
     EXPECT_TRUE(isLibraryOpen(kLib));
 }
 
