a0792f37b2e9f5a2ea05c27a5b0430946ea39c9f
liujinye <liujinye@xiaomi.com>
2019-01-11 17:17:43 +0800

    [automerger skipped] Add gServiceSidMap. am: ca016f9ac4 -s ours am skip reason: change_id I4e5821d91ea694f86911218c23e68fcb987daaf9 with SHA1 d7f8d2226f is in history
    Change-Id: I32c7a57e2f40fa50cd27b5021312a0b0bb1999b9

--- libhidlcache/MemoryDealer.cpp
+++ libhidlcache/MemoryDealer.cpp
@@ -123,7 +123,7 @@ SimpleBestFitAllocator::~SimpleBestFitAllocator() {
         // and generates a false positive warning about accessing
         // memory that is already freed.
         // Add an "assert" to avoid the confusion.
-        LOG_ALWAYS_FATAL_IF(mList.front() == removed);
+        LOG_ALWAYS_FATAL_IF(mList.head() == removed);
 #endif
         delete removed;
     }
@@ -240,7 +240,7 @@ SimpleBestFitAllocator::chunk_t* SimpleBestFitAllocator::dealloc(size_t start) {
             return freed;
         }
     }
-    return nullptr;
+    return 0;
 }
 
 void SimpleBestFitAllocator::dump(const char* tag) const {
