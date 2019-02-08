1768b650071262a6feac70649474cc0c75a75f6e
liujinye <liujinye@xiaomi.com>
2019-02-09 02:32:33 +0800

    Snap for 5180536 from 3f3ff1aca3ec4d17fd0e6a0d3f23108ddcc95134 to pi-platform-release
    Change-Id: I32ef7c75500029c00ab7acd14cbec5b885d73724

--- transport/include/hidl/ConcurrentMap.h
+++ transport/include/hidl/ConcurrentMap.h
@@ -67,7 +67,6 @@ public:
     std::unique_lock<std::mutex> lock() { return std::unique_lock<std::mutex>(mMutex); }
 
     void setLocked(K&& k, V&& v) { mMap[std::forward<K>(k)] = std::forward<V>(v); }
-    void setLocked(K&& k, const V& v) { mMap[std::forward<K>(k)] = v; }
 
     const V& getLocked(const K& k, const V& def) const {
         const_iterator iter = mMap.find(k);
@@ -76,14 +75,6 @@ public:
         }
         return iter->second;
     }
-
-    size_type eraseLocked(const K& k) { return mMap.erase(k); }
-
-    // the concurrent map must be locked in order to iterate over it
-    iterator begin() { return mMap.begin(); }
-    iterator end() { return mMap.end(); }
-    const_iterator begin() const { return mMap.begin(); }
-    const_iterator end() const { return mMap.end(); }
 
    private:
     mutable std::mutex mMutex;
