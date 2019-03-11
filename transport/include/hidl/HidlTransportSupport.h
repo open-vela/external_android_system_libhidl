7fe9483c4e1d01f11dc30f097d9a6d9ece290215
openvela-robot <openvela-robot@xiaomi.com>
2019-03-11 23:09:23 +0800

    DO NOT MERGE - Merge PPRL.190305.001 into master
    Bug: 127812889
    Change-Id: I13211edda50c7c94a53e7c6d8ed9637235fe1026

--- transport/include/hidl/HidlTransportSupport.h
+++ transport/include/hidl/HidlTransportSupport.h
@@ -85,6 +85,14 @@ bool setMinSchedulerPolicy(const sp<::android::hidl::base::V1_0::IBase>& service
                            int policy, int priority);
 
 /**
+ * Returns whether two interfaces represent the same interface. References to interfaces in the same
+ * process will always be equivalent. However, in order to compare a service that is a proxy to a
+ * different process, its underlying structure may have to be checked.
+ */
+bool interfacesEqual(const sp<::android::hidl::base::V1_0::IBase>& left,
+                     const sp<::android::hidl::base::V1_0::IBase>& right);
+
+/**
  * Sets whether or not this object should request security contexts to be populatd for incoming
  * calls (e.g. with getCallingSid).
  *
@@ -95,14 +103,6 @@ bool setMinSchedulerPolicy(const sp<::android::hidl::base::V1_0::IBase>& service
  * @param requesting whether or not to request sid (default is false)
  */
 bool setRequestingSid(const sp<::android::hidl::base::V1_0::IBase>& service, bool requesting);
-
-/**
- * Returns whether two interfaces represent the same interface. References to interfaces in the same
- * process will always be equivalent. However, in order to compare a service that is a proxy to a
- * different process, its underlying structure may have to be checked.
- */
-bool interfacesEqual(const sp<::android::hidl::base::V1_0::IBase>& left,
-                     const sp<::android::hidl::base::V1_0::IBase>& right);
 
 namespace details {
 
