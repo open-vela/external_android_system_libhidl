e247ee7749e5cb3bc5af85c017009202e9428cf2
openvela-robot <openvela-robot@xiaomi.com>
2021-01-16 00:10:33 +0800

    Snap for 7085329 from adee24d077f082bc8e946e1cd81a2c3830570c56 to rvc-qpr3-release
    Change-Id: I600fad3a97d8a205c0d057d2a72e2885f5fb3e1d

--- transport/ServiceManagement.cpp
+++ transport/ServiceManagement.cpp
@@ -153,36 +153,6 @@ __attribute__((noinline)) static void tryShortenProcessName(const std::string& d
 
 namespace details {
 
-#ifdef ENFORCE_VINTF_MANIFEST
-static constexpr bool kEnforceVintfManifest = true;
-#else
-static constexpr bool kEnforceVintfManifest = false;
-#endif
-
-#ifdef LIBHIDL_TARGET_DEBUGGABLE
-static constexpr bool kDebuggable = true;
-#else
-static constexpr bool kDebuggable = false;
-#endif
-
-static bool* getTrebleTestingOverridePtr() {
-    static bool gTrebleTestingOverride = false;
-    return &gTrebleTestingOverride;
-}
-
-void setTrebleTestingOverride(bool testingOverride) {
-    *getTrebleTestingOverridePtr() = testingOverride;
-}
-
-static inline bool isTrebleTestingOverride() {
-    if (kEnforceVintfManifest && !kDebuggable) {
-        // don't allow testing override in production
-        return false;
-    }
-
-    return *getTrebleTestingOverridePtr();
-}
-
 /*
  * Returns the age of the current process by reading /proc/self/stat and comparing starttime to the
  * current time. This is useful for measuring how long it took a HAL to register itself.
@@ -250,7 +220,6 @@ static void onRegistrationImpl(const std::string& descriptor, const std::string&
     tryShortenProcessName(descriptor);
 }
 
-// only used by prebuilts - should be able to remove
 void onRegistration(const std::string& packageName, const std::string& interfaceName,
                     const std::string& instanceName) {
     return onRegistrationImpl(packageName + "::" + interfaceName, instanceName);
@@ -422,7 +391,8 @@ struct PassthroughServiceManager : IServiceManager1_1 {
 
         dlerror(); // clear
 
-        static std::string halLibPathVndkSp = details::getVndkSpHwPath();
+        static std::string halLibPathVndkSp = android::base::StringPrintf(
+            HAL_LIBRARY_PATH_VNDK_SP_FOR_VERSION, details::getVndkVersionStr().c_str());
         std::vector<std::string> paths = {
             HAL_LIBRARY_PATH_ODM, HAL_LIBRARY_PATH_VENDOR, halLibPathVndkSp,
 #ifndef __ANDROID_VNDK__
@@ -430,7 +400,10 @@ struct PassthroughServiceManager : IServiceManager1_1 {
 #endif
         };
 
-        if (details::isTrebleTestingOverride()) {
+#ifdef LIBHIDL_TARGET_DEBUGGABLE
+        const char* env = std::getenv("TREBLE_TESTING_OVERRIDE");
+        const bool trebleTestingOverride = env && !strcmp(env, "true");
+        if (trebleTestingOverride) {
             // Load HAL implementations that are statically linked
             handle = dlopen(nullptr, dlMode);
             if (handle == nullptr) {
@@ -441,6 +414,7 @@ struct PassthroughServiceManager : IServiceManager1_1 {
                 return;
             }
         }
+#endif
 
         for (const std::string& path : paths) {
             std::vector<std::string> libs = findFiles(path, prefix, ".so");
@@ -539,8 +513,10 @@ struct PassthroughServiceManager : IServiceManager1_1 {
     Return<void> debugDump(debugDump_cb _hidl_cb) override {
         using Arch = ::android::hidl::base::V1_0::DebugInfo::Architecture;
         using std::literals::string_literals::operator""s;
-        static std::string halLibPathVndkSp64 = details::getVndkSpHwPath("lib64");
-        static std::string halLibPathVndkSp32 = details::getVndkSpHwPath("lib");
+        static std::string halLibPathVndkSp64 = android::base::StringPrintf(
+            HAL_LIBRARY_PATH_VNDK_SP_64BIT_FOR_VERSION, details::getVndkVersionStr().c_str());
+        static std::string halLibPathVndkSp32 = android::base::StringPrintf(
+            HAL_LIBRARY_PATH_VNDK_SP_32BIT_FOR_VERSION, details::getVndkVersionStr().c_str());
         static std::vector<std::pair<Arch, std::vector<const char*>>> sAllPaths{
             {Arch::IS_64BIT,
              {
@@ -787,6 +763,28 @@ bool handleCastError(const Return<bool>& castReturn, const std::string& descript
     ALOGW("getService: unable to call into hwbinder service for %s/%s.",
           descriptor.c_str(), instance.c_str());
     return false;
+}
+
+#ifdef ENFORCE_VINTF_MANIFEST
+static constexpr bool kEnforceVintfManifest = true;
+#else
+static constexpr bool kEnforceVintfManifest = false;
+#endif
+
+#ifdef LIBHIDL_TARGET_DEBUGGABLE
+static constexpr bool kDebuggable = true;
+#else
+static constexpr bool kDebuggable = false;
+#endif
+
+static inline bool isTrebleTestingOverride() {
+    if (kEnforceVintfManifest && !kDebuggable) {
+        // don't allow testing override in production
+        return false;
+    }
+
+    const char* env = std::getenv("TREBLE_TESTING_OVERRIDE");
+    return env && !strcmp(env, "true");
 }
 
 sp<::android::hidl::base::V1_0::IBase> getRawServiceInternal(const std::string& descriptor,
