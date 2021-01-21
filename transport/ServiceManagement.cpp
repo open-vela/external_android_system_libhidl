1b3e1cdd8d1ed2c25952d9dcfebb629933d260ff
liujinye <liujinye@xiaomi.com>
2021-01-21 23:27:10 +0800

    Rip out getProcessAgeMs. am: 489e7571db
    Original change: https://android-review.googlesource.com/c/platform/system/libhidl/+/1556385
    
    MUST ONLY BE SUBMITTED BY AUTOMERGER
    
    Change-Id: I343f890d83f8d683c67c9295a8ee249bdd8d07a5

--- transport/ServiceManagement.cpp
+++ transport/ServiceManagement.cpp
@@ -839,13 +839,7 @@ status_t registerAsServiceInternal(const sp<IBase>& service, const std::string& 
 
     if (kEnforceVintfManifest && !isTrebleTestingOverride()) {
         using Transport = IServiceManager1_0::Transport;
-        Return<Transport> transport = sm->getTransport(descriptor, name);
-
-        if (!transport.isOk()) {
-            LOG(ERROR) << "Could not get transport for " << descriptor << "/" << name << ": "
-                       << transport.description();
-            return UNKNOWN_ERROR;
-        }
+        Transport transport = sm->getTransport(descriptor, name);
 
         if (transport != Transport::HWBINDER) {
             LOG(ERROR) << "Service " << descriptor << "/" << name
