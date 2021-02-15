fe4881cf61ad163158d4b1be3b39f2eef2679696
liujinye <liujinye@xiaomi.com>
2021-02-15 23:48:16 +0800

    [LSC] Add LOCAL_LICENSE_KINDS to system/libhidl am: ad2415c261
    Original change: https://android-review.googlesource.com/c/platform/system/libhidl/+/1589043
    
    MUST ONLY BE SUBMITTED BY AUTOMERGER
    
    Change-Id: Ic0db761c0cc176bcb531716a6a46d1f21cb5af36

--- test_main.cpp
+++ test_main.cpp
@@ -17,7 +17,7 @@
 #define LOG_TAG "LibHidlTest"
 
 #pragma clang diagnostic push
-#pragma clang diagnostic fatal "-Wpadded"
+#pragma clang diagnostic error "-Wpadded"
 #include <hidl/HidlInternal.h>
 #include <hidl/HidlSupport.h>
 #pragma clang diagnostic pop
@@ -220,6 +220,24 @@ TEST_F(LibHidlTest, VecInitTest) {
     hidl_vec<int32_t> v3 = {5, 6, 7}; // initializer_list
     EXPECT_EQ(v3.size(), 3ul);
     EXPECT_ARRAYEQ(v3, array, v3.size());
+}
+
+TEST_F(LibHidlTest, VecReleaseTest) {
+    // this test indicates an inconsistency of behaviors which is undesirable.
+    // Perhaps hidl-vec should always allocate an empty vector whenever it
+    // exposes its data. Alternatively, perhaps it should always free/reject
+    // empty vectors and always return nullptr for this state. While this second
+    // alternative is faster, it makes client code harder to write, and it would
+    // break existing client code.
+    using android::hardware::hidl_vec;
+
+    hidl_vec<int32_t> empty;
+    EXPECT_EQ(nullptr, empty.releaseData());
+
+    empty.resize(0);
+    int32_t* data = empty.releaseData();
+    EXPECT_NE(nullptr, data);
+    delete data;
 }
 
 TEST_F(LibHidlTest, VecIterTest) {
